6a86caa0c25181c6d4f264a66ea9ce5bab2f4ffa
iamyogurthh <saieainkhant992003@gmail.com>
2026-03-21 00:46:31 +0630

    Inatial Setup

---
+++ server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+const expressLayouts = require('express-ejs-layouts');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+
+app.use(express.static('public'));
+
+//db connection
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+//routing
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
